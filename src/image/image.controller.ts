2fc358d64dd6b2ed0be4d48deac8f9ae5af817e4
NestZ <NestZ671@gmail.com>
2021-03-15 20:08:18 +0700

    add exception handler

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -20,26 +20,34 @@ export class ImageController {
   @Post('upload/img')
   @UseInterceptors(FileInterceptor('img', { dest: './upload/img' }))
   async uploadImg(@UploadedFile() file: any, @Res() res: any) {
-    if (file) {
-      const img = await this.imageService.save(file);
-      this.imageService.rm(file['filename']);
-      return res
-        .status(HttpStatus.OK)
-        .json({ status: HttpStatus.OK, _id: img._id });
-    } else
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ status: HttpStatus.BAD_REQUEST });
+    try {
+      if (file) {
+        const img = await this.imageService.save(file);
+        this.imageService.rm(file['filename']);
+        return res
+          .status(HttpStatus.OK)
+          .json({ status: HttpStatus.OK, _id: img._id });
+      } else
+        return res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ status: HttpStatus.BAD_REQUEST });
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json(e);
+    }
   }
 
   @Get('img')
   async getImg(@Query('_id') _id: string, @Res() res: Response) {
-    const loadedImg = await this.imageService.load(new Types.ObjectId(_id));
-    const img = Buffer.from(loadedImg.img.buffer);
-    res.writeHead(200, {
-      'Content-Type': loadedImg.contentType,
-      'Content-Length': img.length,
-    });
-    res.end(img);
+    try {
+      const loadedImg = await this.imageService.load(new Types.ObjectId(_id));
+      const img = Buffer.from(loadedImg.img.buffer);
+      res.writeHead(200, {
+        'Content-Type': loadedImg.contentType,
+        'Content-Length': img.length,
+      });
+      res.end(img);
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).json(e);
+    }
   }
 }
